5f3e2a3b43618a7821b3cf1f0e18aa6155c60121
Sean Tracey <seanmtracey@gmail.com>
2017-03-31 15:06:54 +0100

    Connect to database and output info.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
+const debug = require('debug')('audio-management:routes:index');
 const express = require('express');
 const router = express.Router();
 
+const database = require('../bin/lib/database');
+const generateS3URL = require('../bin/lib/generate-public-s3-url');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+	database.scan({
+			TableName : process.env.AWS_AUDIO_METADATA_TABLE,
+			FilterExpression : 'attribute_exists(#uuid)',
+			ExpressionAttributeNames : {
+				'#uuid': 'uuid'
+			}
+		
+		})
+		.then(data => {
+
+			const readiedAssets = data.Items.map(item => {
+				item.publicURL = generateS3URL(item.uuid);
+				return item;
+			});
+
+			res.render('index', { 
+				title: 'Express',
+				audioAssets : Array.from(readiedAssets)
+			});
+
+		})
+		.catch(err => {
+			res.render('error', err);
+		});
+	;
+
+
 });
+
 
 module.exports = router;

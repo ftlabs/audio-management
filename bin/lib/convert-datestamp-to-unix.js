0442d8f4f7dcc2ceee1d3ee6b6d60a7db1d3a43f
Sean Tracey <seanmtracey@gmail.com>
2017-04-03 17:24:21 +0100

    Add S3O.

--- bin/lib/convert-datestamp-to-unix.js
+++ bin/lib/convert-datestamp-to-unix.js
@@ -2,7 +2,5 @@ const debug = require('debug')('bin:lib:convert-datestamp-to-unix');
 const moment = require('moment');
 
 module.exports = function(dateStamp, stampFormat='ddd, DD MMM YYYY HH:mm:ss ZZ'){
-
 	return moment(dateStamp, stampFormat).valueOf();
-
 };	

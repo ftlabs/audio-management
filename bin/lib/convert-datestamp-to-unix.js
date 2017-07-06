74f0a2bce6f3b1d179789017b3b7c999f2721a27
Sean Tracey <seanmtracey@gmail.com>
2017-07-06 12:18:42 +0100

    Debug timestamp conversion.

--- bin/lib/convert-datestamp-to-unix.js
+++ bin/lib/convert-datestamp-to-unix.js
@@ -2,5 +2,10 @@ const debug = require('debug')('bin:lib:convert-datestamp-to-unix');
 const moment = require('moment');
 
 module.exports = function(dateStamp, stampFormat='ddd, DD MMM YYYY HH:mm:ss ZZ'){
-	return moment(dateStamp, stampFormat).valueOf();
+	
+	debug(`Converting dateStamp (${dateStamp}) to unix time`);
+	const unixValue = moment(dateStamp, stampFormat).valueOf();
+	debug(`${dateStamp} -> ${unixValue}`);
+	return unixValue;
+	
 };	
